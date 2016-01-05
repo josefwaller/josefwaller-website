ec3045550ba287488daf0aa433d942877e601f37
josefwaller <josef@siriusapplications.com>
2016-01-05 13:22:37 -0800

    Set up for heroku push

--- josefwaller_website/mainsite/models.py
+++
@@ -1,9 +0,0 @@
-from django.db import models
-
-# Create your models here.
-
-class EvolutionSave (models.Model):
-
-	starting_coords = models.CharField(max_length=6)
-	asteroids = models.CharField(max_length=200)
-	genes = models.CharField(max_length=300)
