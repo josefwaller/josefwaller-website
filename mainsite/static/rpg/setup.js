674e7ecae68821e8287426c293a4af467c51d6d7
josefwaller <josef@siriusapplications.com>
2016-01-19 11:07:49 -0800

    Added art page colors, grid

--- mainsite/static/rpg/setup.js
+++ mainsite/static/rpg/setup.js
@@ -1,6 +1,26 @@
 var sprites;
 var background;
+
+var screens;
+
+var colors = [
+	"#aaaaaa",
+	"#000000",
+	"#ff0000",
+	"#00ff00",
+	"#0000ff",
+	"#ffff00",
+	"#00ffff",
+	"#ff00ff"
+]
+
+var currentScreen;
 var art;
+
+var mousePos = {
+	x:0,
+	y:0
+}
 
 function setup () {
 	
@@ -35,10 +55,47 @@ function setup () {
 	}
 
 	// Sets up art canvas
-	art = Art();
+	art = Art(size);
+
+	// Sets up changing screens
+	$("#level-editor-btn").click({i:0}, changeScreen)
+	$("#art-btn").click({i:1}, changeScreen)
+	$("#music-btn").click({i:2}, changeScreen)
+
+	// Sets screens
+	screens = {
+		levelEditor: $("#level-editor"),
+		art: $("#art"),
+		music: $("#music")
+	}
+
+	$(document).mousemove(function(event) {
+		mousePos.x = event.pageX,
+		mousePos.y = event.pageY
+	})
 
 	window.setInterval(update, 1000/60)
 
+}
+
+function changeScreen(event) {
+	i = event.data.i;
+	screens.levelEditor.hide();
+	screens.art.hide();
+	screens.music.hide();
+
+	switch(i){
+		case 0:
+			screens.levelEditor.show();
+			console.log("ASDF")
+			break;
+		case 1:
+			screens.art.show();
+			break;
+		case 2:
+			screens.music.show();
+			break;
+	}
 }
 
 function update() {
