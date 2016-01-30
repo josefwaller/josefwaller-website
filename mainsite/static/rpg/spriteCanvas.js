c161961bc9b1041694df0dada1244eca272bd37e
josefwaller <josef@isaacwaller.com>
2016-01-30 12:43:22 -0800

    Fixed the preview images eing blurred

--- mainsite/static/rpg/spriteCanvas.js
+++ mainsite/static/rpg/spriteCanvas.js
@@ -5,6 +5,9 @@ var spriteCanvas = Class({
 
 	image: 0,
 
+	// increaes the resolution to avoid blur
+	res_mulitplier: 3,
+
 	init: function(p) {
 
 		this.image = sprites[p.object][p.sprite];
@@ -12,8 +15,8 @@ var spriteCanvas = Class({
 		this.canvas = p.canvas;
 		this.ctx = this.canvas[0].getContext("2d");
 
-		this.canvas[0].width = this.image.length;
-		this.canvas[0].height = this.image.length;
+		this.canvas[0].width = this.image.length * this.res_mulitplier;
+		this.canvas[0].height = this.image.length * this.res_mulitplier;
 	},
 
 	draw: function() {
@@ -21,12 +24,16 @@ var spriteCanvas = Class({
 		ctx = this.ctx;
 		ctx.clearRect(0, 0, this.canvas[0].width, this.canvas[0].height);
 
-		for (x = 0; x < this.canvas[0].width; x++){
-			for (y = 0; y < this.canvas[0].height; y++){
+		for (x = 0; x < this.image.length; x++){
+			for (y = 0; y < this.image[x].length; y++){
 
 				if (this.image[x][y] !== null){
 					ctx.fillStyle = colors[this.image[x][y]].hex;
-					ctx.fillRect(x, y, 1, 1);
+					ctx.fillRect(
+						x * this.res_mulitplier, 
+						y * this.res_mulitplier, 
+						this.res_mulitplier, 
+						this.res_mulitplier);
 				}
 			}
 		}
